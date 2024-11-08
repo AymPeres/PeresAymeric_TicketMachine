a170eef72f872a7b76c45dd109d7ddda62657792
AymPeres <aymeric.peres@orange.fr>
2024-11-08 15:55:48 +0100

    test comit complete

--- src/main/java/ticketmachine/TicketMachine.java
+++ src/main/java/ticketmachine/TicketMachine.java
@@ -23,6 +23,7 @@ public class TicketMachine {
 	 */
 	public TicketMachine(int ticketCost) {
 		// Test de validité du paramètre
+
 		if (ticketCost <= 0) {
 			throw new IllegalArgumentException("Ticket price must be positive");
 		}
@@ -63,6 +64,9 @@ public class TicketMachine {
 	 * @throws IllegalArgumentException if amount is not positive
 	 */
 	public void insertMoney(int amount) {
+		if (amount <= 0) {
+			throw new IllegalArgumentException("Insert amount must be positive");
+		}
 		balance = balance + amount;
 	}
 
@@ -73,22 +77,28 @@ public class TicketMachine {
 	 */
 	public int refund() {
 		System.out.println("Je vous rends : " + balance + " centimes");
-		return balance;
+		int toRefund = balance;
+		balance = 0;
+		return toRefund;
 	}
 
 	/**
-	 * Print a ticket. Update the total collected and reduce the balance 
+	 * Print a ticket. Update the total collected and reduce the balance
 	 *
 	 * @return vrai si le ticket a été imprimé, faux sinon
 	 */
 	public boolean printTicket() {
-		// Simulate the printing of a ticket.
-		System.out.println("##################");
-		System.out.println("# The BlueJ Line");
-		System.out.println("# Ticket");
-		System.out.println("# " + price + " cents.");
-		System.out.println("##################");
-		System.out.println();
-		return true;
+		if (balance >= price){
+			// Simulate the printing of a ticket.
+			System.out.println("##################");
+			System.out.println("# The BlueJ Line");
+			System.out.println("# Ticket");
+			System.out.println("# " + price + " cents.");
+			System.out.println("##################");
+			System.out.println();
+			balance -= price;
+			return true;
+		}
+		return false;
 	}
 }
